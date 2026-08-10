d9eb1616127735ca85f7af9ec07a8f52923ba15b
WuYaTanStrawberry <sabina851125@gmail.com>
2026-08-10 23:21:35 +0800

    後端改用自訂網域 api.wuyatanstrawberry.com(可套 WAF 速率限制)

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 後端 API 位址(Cloudflare Worker)
 // 舊的 Google Apps Script 版:https://script.google.com/macros/s/AKfycbyFta05.../exec(已退役備用)
 window.APP_CONFIG = {
-  apiBase: 'https://strawberry-booking.wuyatan.workers.dev',
+  apiBase: 'https://api.wuyatanstrawberry.com',
 };
